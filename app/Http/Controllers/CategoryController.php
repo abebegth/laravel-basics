a95dbb2b04baa84f93ded6fa7344dc989b3655dd
Abebe Gietaneh <abebegth@gmail.com>
2021-12-16 21:43:43 +0300

    Insert data into the Category Modle / categories table using eloquent ORM

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
+use Auth;
+use Illuminate\Support\Carbon;
 
 class CategoryController extends Controller
 {
@@ -18,5 +21,18 @@ class CategoryController extends Controller
             'category_name.required' =>"Please enter category name"
         ]
         );
+
+        Category::insert([
+            'category_name'=>$request->category_name,
+            'user_id'=>Auth::user()->id,
+            'created_at'=>Carbon::now()
+        ]);
+
+        // $category = new Category;
+        // $category->category_name = $request->category_name;
+        // $category->user_id = Auth::user()->id;
+        // $category->save();
+
+        return Redirect()->back()->with('success', "Category inserted successfully");
     }
 }
