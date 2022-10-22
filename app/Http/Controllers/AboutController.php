d181641fe5208990a5f5de4579b4a9326ef265b8
Abebe Gietaneh <abebegietaneh@gmail.com>
2022-10-22 13:33:16 +0300

    Create the About data

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -4,11 +4,28 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\About;
+use Illuminate\Support\Carbon;
 
 class AboutController extends Controller
 {
     public function about(){
         $abouts = About::latest()->get();
         return view('admin.about.index', compact('abouts'));
+    }
+
+    public function addAbout(){
+        return view('admin.about.create');
+    }
+
+    public function storeAbout(Request $request){
+
+        About::insert([
+            'title' => $request->title,
+            'summary' => $request->summary,
+            'detail' => $request->detail,
+            'created_at'=>Carbon::now()
+        ]);
+
+        return Redirect()->route('about')->with('success', "About inserted successfully");
     }
 }
